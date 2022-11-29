77cdd39ae6ae9265b0b260bd75f77add14f5c2f5
Alan <alanhussein116@gmail.com>
2022-11-29 20:23:49 +0100

    Homework week1 Database

---
+++ Week1/exercise_2.js
@@ -0,0 +1,29 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "world",
+});
+
+connection.connect();
+
+const queries = [
+  "SELECT name, population FROM country WHERE population > 8000000",
+  'SELECT name FROM country WHERE name LIKE "%land%"',
+  "SELECT name , population FROM city WHERE population BETWEEN 500000 AND 1000000",
+  'SELECT name FROM country WHERE Continent = "Europe"',
+  "SELECT * FROM country ORDER BY surfaceArea DESC",
+  'SELECT name, population FROM city WHERE name = "Rotterdam"',
+  "SELECT name, population FROM city ORDER BY population DESC LIMIT 10",
+  "SELECT SUM(population) FROM country",
+];
+
+queries.forEach((el) => {
+  connection.query(el, (err, results) => {
+    if (err) throw err;
+    results.forEach((res) => console.log(res));
+  });
+});
+connection.end();
---
+++ Week1/exrecise_1.js
@@ -0,0 +1,58 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "meetup",
+});
+
+connection.connect();
+connection.query(`DROP TABLE Invitee, Room, Meeting`);
+
+connection.query(`CREATE TABLE Invitee (
+      invitee_no INT PRIMARY KEY, 
+      invitee_name VARCHAR(64), 
+      invited_by VARCHAR(64)
+    )`);
+
+connection.query(`CREATE TABLE Room (
+    room_no INT PRIMARY KEY, 
+    room_name VARCHAR(64) , 
+    floor_number INT
+  )`);
+
+connection.query(`CREATE TABLE Meeting (
+    meeting_no INT PRIMARY KEY,
+    meeting_title VARCHAR(64), 
+    starting_time DATETIME, 
+    ending_time DATETIME,
+    room_no INT, 
+    FOREIGN KEY (room_no) REFERENCES Room(room_no)
+  )`);
+
+connection.query(
+  `INSERT INTO Invitee 
+  VALUE (1,'Alan','Hussain'),
+    (2,'Joe','Doe'),
+    (3,'Alex','Pol'),
+    (4,'Max','Ston'),
+    (5,'Sara','Hussain'),`
+);
+connection.query(
+  `INSERT INTO Room VALUE
+    (1,'Problem-solving Room',4),
+    (2,'Decision-making Room',5),
+    (3,'planning Room',2),
+    (4,'Feedback Room',1),
+    (5,'Status Room',7),`
+);
+connection.query(
+  `INSERT INTO Meeting VALUE
+    (1,'Problem-solving meetings','2022-11-30 12:00:00',4),
+    (2,'Decision-making meetings','2022-12-1 13:00:00',5),
+    (3,'Quarterly planning meetings','2022-12-7 14:30:00',2),
+    (4,'Feedback and retrospective meetings','2022-12-15 11:00:00',1),
+    (5,'Status update meetings','2022-12-20 11:30:00',7),`
+);
+connection.end();
