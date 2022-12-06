7f6899fb2591e79502435a61a931ccb1f7319a9c
Alan <alanhussein116@gmail.com>
2022-12-06 14:26:51 +0100

    fixed the errors

--- Week1/exrecise_1.js
+++ Week1/exrecise_1.js
@@ -7,22 +7,22 @@ const connection = mysql.createConnection({
   database: "meetup",
 });
 
-connection.connect();
-connection.query(`DROP TABLE Invitee, Room, Meeting`);
+connection.connect(() => {
+  connection.query(`DROP TABLE Invitee, Room, Meeting`);
 
-connection.query(`CREATE TABLE Invitee (
+  connection.query(`CREATE TABLE Invitee (
       invitee_no INT PRIMARY KEY, 
       invitee_name VARCHAR(64), 
       invited_by VARCHAR(64)
     )`);
 
-connection.query(`CREATE TABLE Room (
+  connection.query(`CREATE TABLE Room (
     room_no INT PRIMARY KEY, 
     room_name VARCHAR(64) , 
     floor_number INT
   )`);
 
-connection.query(`CREATE TABLE Meeting (
+  connection.query(`CREATE TABLE Meeting (
     meeting_no INT PRIMARY KEY,
     meeting_title VARCHAR(64), 
     starting_time DATETIME, 
@@ -31,28 +31,29 @@ connection.query(`CREATE TABLE Meeting (
     FOREIGN KEY (room_no) REFERENCES Room(room_no)
   )`);
 
-connection.query(
-  `INSERT INTO Invitee 
-  VALUE (1,'Alan','Hussain'),
+  connection.query(
+    `INSERT INTO Invitee 
+  Value (1,'Alan','Hussain'),
     (2,'Joe','Doe'),
     (3,'Alex','Pol'),
     (4,'Max','Ston'),
-    (5,'Sara','Hussain'),`
-);
-connection.query(
-  `INSERT INTO Room VALUE
-    (1,'Problem-solving Room',4),
+    (5,'Sara','Hussain');`
+  );
+  connection.query(
+    `INSERT INTO Room 
+    value (1,'Problem-solving Room',4),
     (2,'Decision-making Room',5),
     (3,'planning Room',2),
     (4,'Feedback Room',1),
-    (5,'Status Room',7),`
-);
-connection.query(
-  `INSERT INTO Meeting VALUE
-    (1,'Problem-solving meetings','2022-11-30 12:00:00',4),
-    (2,'Decision-making meetings','2022-12-1 13:00:00',5),
-    (3,'Quarterly planning meetings','2022-12-7 14:30:00',2),
-    (4,'Feedback and retrospective meetings','2022-12-15 11:00:00',1),
-    (5,'Status update meetings','2022-12-20 11:30:00',7),`
-);
-connection.end();
+    (5,'Status Room',5);`
+  );
+  connection.query(
+    `INSERT INTO Meeting
+   value (1,'Problem-solving meetings','2022-11-30 12:00:00','2022-11-30 13:30:00',4),
+    (2,'Decision-making meetings','2022-12-1 13:00:00','2022-12-1 14:00:00',5),
+    (3,'Quarterly planning meetings','2022-12-7 14:30:00','2022-12-7 15:30:00',2),
+    (4,'Feedback and retrospective meetings','2022-12-15 11:00:00','2022-12-15 12:00:00',1),
+    (5,'Status update meetings','2022-12-20 11:30:00','2022-12-20 13:30:00',5);`
+  );
+  connection.end();
+});
